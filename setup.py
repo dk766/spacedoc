44f6ec1c71d065ced62a4817b929580415365847
Chifu Valerian <vchifu@gmail.com>
2018-02-20 23:56:17 +0100

    Setting up the project

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from spacedoc import __version__
+
+setup(
+    name='spacedoc',
+    version=__version__,
+
+    url='http://host/',
+    author='dk766',
+    author_email='vchifu@gmail.com',
+
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=['scripts/manage.py'],
+
+    install_requires=(
+        'django<1.7',
+    )
+)
